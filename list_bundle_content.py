ca07b1abe38039f329ddd38bb7e1a7b9c8962250
Wok <woctezuma@users.noreply.github.com>
2022-03-20 23:15:34 +0100

    Add script to list bundle content

---
+++ list_bundle_content.py
@@ -0,0 +1,18 @@
+from src.fetch_utils import fetch_bundle_page_with_given_slug
+from src.soup_utils import extract_game_items, extract_metadata_for_all_games
+
+
+def main():
+    bundle_slug = "humble-stand-with-ukraine-bundle"
+    page_soup = fetch_bundle_page_with_given_slug(bundle_slug)
+
+    game_items = extract_game_items(page_soup=page_soup)
+    metadata = extract_metadata_for_all_games(game_items=game_items)
+
+    print(metadata)
+
+    return True
+
+
+if __name__ == "__main__":
+    main()
