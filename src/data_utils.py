fb0865b08f41f32e2936b174cd9386e63bc32c03
Wok <woctezuma@users.noreply.github.com>
2022-03-21 12:46:00 +0100

    Add data utils to store paths to data files

---
+++ src/data_utils.py
@@ -0,0 +1,23 @@
+import pathlib
+
+
+def get_data_folder():
+    data_folder = "data/"
+    pathlib.Path(data_folder).mkdir(exist_ok=True)
+    return data_folder
+
+
+def get_json_file_extension():
+    return ".json"
+
+
+def get_file_path(base_fname):
+    return get_data_folder() + base_fname + get_json_file_extension()
+
+
+def get_bundle_fname(bundle_slug):
+    return get_file_path(base_fname=bundle_slug)
+
+
+def get_price_fname():
+    return get_file_path(base_fname="prices")
