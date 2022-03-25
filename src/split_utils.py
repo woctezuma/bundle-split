232c05eb8dfdee28298260224acd0198e219f1c8
Wok <woctezuma@users.noreply.github.com>
2022-03-25 19:46:20 +0100

    Add utils to compute split prices based on the ask

---
+++ src/split_utils.py
@@ -0,0 +1,52 @@
+from src.parse_utils import get_currency_symbol
+
+
+def get_keyshop_label():
+    return "#keyshops"
+
+
+def get_ask_price_for_single_product(product_metadata):
+    keyshop_label = get_keyshop_label()
+
+    try:
+        ask_price = product_metadata[keyshop_label]
+    except KeyError:
+        ask_price = 0
+
+    return ask_price
+
+
+def get_ask_prices_for_whole_bundle(price_metadata):
+    ask_prices = {}
+
+    for (slug, product_metadata) in price_metadata.items():
+        ask_prices[slug] = get_ask_price_for_single_product(product_metadata)
+
+    return ask_prices
+
+
+def round_price(price):
+    num_decimal_digits = 2
+    return round(price, num_decimal_digits)
+
+
+def get_bundle_value(ask_prices):
+    return round_price(sum(ask_prices.values()))
+
+
+def split_bundle_cost(ask_prices, target_cost, verbose=True):
+    bundle_value = get_bundle_value(ask_prices)
+    cost_value_ratio = target_cost / bundle_value
+
+    if verbose:
+        print(f"Bundle value: {get_bundle_value(ask_prices)} {get_currency_symbol()}")
+
+    split_prices = {}
+
+    for (slug, ask) in ask_prices.items():
+        split_prices[slug] = round_price(ask * cost_value_ratio)
+
+    if verbose:
+        print(f"Bundle cost: {get_bundle_value(split_prices)} {get_currency_symbol()}")
+
+    return split_prices
