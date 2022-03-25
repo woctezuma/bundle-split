9c90f53b65e33f15d85a65438f9b3ffb4bdf3def
Wok <woctezuma@users.noreply.github.com>
2022-03-25 20:19:18 +0100

    Add utils to display split prices

---
+++ src/print_utils.py
@@ -0,0 +1,29 @@
+from src.parse_utils import get_currency_symbol
+from src.split_utils import get_bundle_value
+
+
+def print_line_separator():
+    sep = "="
+    num_sep = 11
+
+    print(sep * num_sep)
+
+    return
+
+
+def print_prices(bundle_metadata, prices):
+    currency = get_currency_symbol()
+
+    print("\nAvailable:")
+    print_line_separator()
+
+    for slug in sorted(prices, key=lambda x: prices[x], reverse=True):
+        title = bundle_metadata[slug]["title"]
+        price = prices[slug]
+
+        print(f"☐ {title} - {price:.2f} {currency}")
+
+    print_line_separator()
+    print(f"Total: {get_bundle_value(prices)} {currency}")
+
+    return
