c4b7fc1dd4544ce00144b6a5da3b54cd5138f4b9
Wok <woctezuma@users.noreply.github.com>
2023-02-04 00:38:47 +0100

    Refactoring: explicitly fetch the page soup in main()

--- run_workflow.py
+++ run_workflow.py
@@ -1,10 +1,12 @@
 import list_bundle_content
 import list_product_prices
 import split_bundle_cost
+from src.fetch_utils import fetch_bundle_page_with_given_slug
 
 
 def main(bundle_slug, target_cost_in_euros):
-    list_bundle_content.main(bundle_slug)
+    page_soup = fetch_bundle_page_with_given_slug(bundle_slug)
+    list_bundle_content.main(bundle_slug, page_soup=page_soup)
     list_product_prices.main(bundle_slug)
     split_bundle_cost.main(bundle_slug, target_cost_in_euros)
 
