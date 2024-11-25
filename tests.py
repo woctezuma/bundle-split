53bca8c3b7d311e9b9f3d92c0ebb55fe0945e20a
Wok <woctezuma@users.noreply.github.com>
2024-11-25 23:20:16 +0100

    Use static method in tests

--- tests.py
+++ tests.py
@@ -5,7 +5,8 @@ from src.soup_prices import extract_metadata_for_all_prices, extract_price_items
 
 
 class TestSoupUtilsMethods(TestCase):
-    def test_price_of_unreleased_product(self) -> None:
+    @staticmethod
+    def test_price_of_unreleased_product() -> None:
         # This tests what happens when prices are marked as "Unavailable".
         # Reference: https://gg.deals/game/hollow-knight-silksong/
         href = "/game/hollow-knight-silksong/"
